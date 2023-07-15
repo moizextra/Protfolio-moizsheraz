fd8362f5d888301d457c91cdc5c4491bfdf30b77
abdulmoizsheraz <moiz77131@gmail.com>
2023-07-15 15:19:39 +0500

    res

--- src/Componets/About.jsx
+++ src/Componets/About.jsx
@@ -3,7 +3,7 @@ import {RiContactsLine} from "react-icons/ri"
 const About = () => {
   return (
     <div className='flex flex-col items-center justify-center '>
- <span className='border px-4 py-1 rounded-full text-center flex items-center justify-center font-bold'><RiContactsLine/> <span className='ml-3'>About</span></span>
+ <span className=' abouttag border px-4 py-1 rounded-full text-center flex items-center justify-center font-bold'><RiContactsLine/> <span className='ml-3'>About</span></span>
         <p className=' lg:mt-10 mb-5 text-gray-300 font-bold aboutpara sm:text-xl mr-0 ml-0 mt-0'>Hello, I'm Abdul Moiz Sheraz, currently pursuing a Bachelor's degree in Computer Science at UET Taxila. As a first-year student, I'm eager to dive into the world of software development and explore its endless possibilities. I have a strong command of languages like JavaScript and Java, and I'm particularly proficient in working with the MERN stack, utilizing technologies like React for building dynamic and responsive web applications. I am motivated to continuously enhance my skills and stay up-to-date with the latest industry trends. I'm excited to apply my knowledge and contribute to meaningful projects that make a difference.</p>
     </div>
   )
--- src/Componets/Skills.jsx
+++ src/Componets/Skills.jsx
@@ -9,7 +9,7 @@ import {SiTypescript} from "react-icons/si"
 const Skills = () => {
   return (
     <div className='flex flex-col justify-center items-center mt-4' data-aos="fade-up">
-       <span className='border px-4 py-1 rounded-full text-center flex items-center justify-center mb-12'><SiTalenthouse/> <span className='ml-3'>Skills</span></span>
+       <span className='border px-4 py-1 rounded-full text-center flex items-center justify-center mb-12 skillstag'><SiTalenthouse/> <span className='ml-3'>Skills</span></span>
        <div className=' skillsdiv flex flex-wrap '>
        <div className=' skill flex roundcustom m-2 customborderaduis flex-col w-48 justify-center items-center border  p-9 '>
         <span className='text-4xl mb-2 skillname'>React</span>
