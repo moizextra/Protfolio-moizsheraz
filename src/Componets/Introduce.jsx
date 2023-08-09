49918ce2797f2dc88f33ef5a23b19a036ed1e08e
abdulmoizsheraz <moiz77131@gmail.com>
2023-08-09 20:54:02 +0500

    someUIfixes

--- src/Componets/Introduce.jsx
+++ src/Componets/Introduce.jsx
@@ -5,7 +5,7 @@ const Introduce = () => {
   return (
     <div className='flex flex-col  ' data-aos="fade-up">
  
-        <span className=' lg:text-6xl  leading-normal font-serif intropara'>Hi there &#128075; ! I am <span style={{"color":"#2272FF"}}>Ｍｏｉｚ</span>,<br/> Developer and <br/> Student </span><br/>
+        <span className=' lg:text-6xl  leading-normal font-serif intropara'>Hi there &#128075; ! I am <span style={{"color":"#2272FF"}}>𝕄𝕠𝕚𝕫</span>,<br/> Developer and <br/> Student </span><br/>
     </div>
   )
 } 
